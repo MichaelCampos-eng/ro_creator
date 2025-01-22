03349cb10d425d7ecd8057d13d13294f575f541c
MichaelCampos-eng <michael_c55@berkeley.edu>
2025-01-22 11:45:25 -0700

    setup change

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
   
 setup( 
     name='wrman', 
-    version='0.1', 
+    version='1.0', 
     description='Maps wiring connections', 
     author='Michael Campos', 
     author_email='michael_c55@berkeley.edu', 
-    packages=find_packages(), 
     install_requires=[ 
         'hydra-core', 
         'pandas',
