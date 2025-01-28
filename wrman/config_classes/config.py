cb3dacc16d1262481de575a64f49ad17cd3bff8e
MichaelCampos-eng <michael_c55@berkeley.edu>
2025-01-27 20:29:48 -0700

    ok

--- wrman/config_classes/config.py
+++ wrman/config_classes/config.py
@@ -3,14 +3,24 @@ from typing import Optional, Union
 
 @dataclass
 class TestConfig:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
     execute: True
     block_name: str
     param: str
 
 @dataclass
 class Config:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if isinstance(value, dict):
+                setattr(self, key, TestConfig(**value))  # Convert dict to OK object
+            else:
+                setattr(self, key, value)
+                
     results_path: str
-
     continuity_cfg: TestConfig
     leakage_cfg: TestConfig
     isolation_cfg: TestConfig
